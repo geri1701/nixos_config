f7d2d61b6a5f842627d3392fc1bd0c71deddbb8a
geri1701 <gschwa@gmx.net>
2023-03-11 01:22:11 +0100

    adds hyprland to imports

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
     sops-nix.url = "github:Mic92/sops-nix";
   };
@@ -18,13 +18,16 @@
           {
             programs.hyprland.enable = true;
           }
-          # sops-nix.nixosModules.sops
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               extraSpecialArgs = { inherit inputs; };
-              users.geri.imports = [./home.nix sops-nix.homeManagerModules.sops];
+              users.geri.imports = [
+                ./home.nix
+                sops-nix.homeManagerModules.sops
+                hyprland.homeManagerModules.default
+              ];
             };
           }
         ];
