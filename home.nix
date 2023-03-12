7fd8abc41f4318beea7d4cb06a53719d540569f9
geri1701 <geri@sdf.org>
2023-03-13 00:02:22 +0100

    remove unnecessary lines from config file

--- home.nix
+++ home.nix
@@ -39,8 +39,7 @@ in {
       exec-once=dunst
       exec-once=waybar 
       exec-once=hyprpaper
-      exec-once=steam -no-browser
-
+      exec-once=steam
       input {
           kb_file=
           kb_layout= us
@@ -48,30 +47,20 @@ in {
           kb_model=
           kb_options= caps:escape
           kb_rules=
-
           follow_mouse=1
-
           touchpad {
               natural_scroll=no
           }
-
           sensitivity=0 # -1.0 - 1.0, 0 means no modification.
       }
-
       general {
-          # main_mod=SUPER
-
           gaps_in=6
           gaps_out=15
           border_size=0
           col.active_border= 0x44FF7F00
           col.inactive_border=0x66333333
-
-          apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
-
-        #  damage_tracking=full # leave it on full unless you hate your GPU and want to make it suffer
+          apply_sens_to_raw=0 
       }
-
       decoration {
           rounding=15
           blur=1
@@ -79,9 +68,7 @@ in {
           blur_passes=1 # minimum 1
           blur_new_optimizations=1
       }
-
       bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
       animations {
           enabled=1
           #animation=windows,1,2,default
@@ -89,37 +76,18 @@ in {
           animation=fade,1,3,default
           animation=workspaces,1,6,default
           animation=windows,1,8,myBezier, popin 80%
-
       }
-
       dwindle {
           pseudotile=0 # enable pseudotiling on dwindle
       }
-
       gestures {
           workspace_swipe=no
       }
-
-      # example window rules
-      # for windows named/classed as abc and xyz
-      #windowrule=move 69 420,abc
-      #windowrule=size 420 69,abc
-      #windowrule=tile,xyz
-      #windowrule=float,abc
-      #windowrule=pseudo,abc
-      #windowrule=monitor 0,xyz
-      #
-      #
-      #
-      # for floating chrome save file dialogs 
       windowrule=float,^(xdg-desktop-portal-gnome)$
       windowrule=float,^(Rofi)$
       windowrule=float,^(jetbrains-studio)$
-      # some nice mouse binds
       bindm=SUPER,mouse:272,movewindow
       bindm=SUPER,mouse:273,resizewindow
-
-      # example binds
       bind=SUPER,Q,exec,wezterm
       bind=SUPER,RETURN,exec,wezterm
       bind=SUPER,C,killactive,
@@ -130,12 +98,10 @@ in {
       bind=SUPER,F,togglefloating,
       bind=ALT,SPACE,exec,~/.config/rofi/bin/app-launcher.sh
       bind=SUPER,V,pseudo
-
       bind=SUPER,h,movefocus,l
       bind=SUPER,l,movefocus,r
       bind=SUPER,k,movefocus,u
       bind=SUPER,j,movefocus,d
-
       bind=SUPER,1,workspace,1
       bind=SUPER,2,workspace,2
       bind=SUPER,3,workspace,3
@@ -146,7 +112,6 @@ in {
       bind=SUPER,8,workspace,8
       bind=SUPER,9,workspace,9
       bind=SUPER,0,workspace,10
-
       bind=ALT,1,movetoworkspace,1
       bind=ALT,2,movetoworkspace,2
       bind=ALT,3,movetoworkspace,3
@@ -157,12 +122,10 @@ in {
       bind=ALT,8,movetoworkspace,8
       bind=ALT,9,movetoworkspace,9
       bind=ALT,0,movetoworkspace,10
-
       bind=SUPER,mouse_down,workspace,e+1
       bind=SUPER,mouse_up,workspace,e-1
-      bind = SUPER,Tab,cyclenext,          # change focus to another window
-      bind = SUPER,Tab,bringactivetotop,   # bring it to the top
-
+      bind = SUPER,Tab,cyclenext,          # change focus
+      bind = SUPER,Tab,bringactivetotop,   # to top
     '';
   };
   xdg.enable = true;
