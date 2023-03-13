9bbd45b592420975e4258801d50aa1f46acb408b
geri1701 <geri@sdf.org>
2023-03-13 23:23:26 +0100

    remove unnecessary comments

--- configuration.nix
+++ configuration.nix
@@ -224,8 +224,6 @@ in {
     wget
     zoxide
   ];
-  ## DEFAULT shell
-  #
   users.defaultUserShell = pkgs.zsh;
   security.polkit.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
@@ -237,11 +235,7 @@ in {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  # e.g. man configuration.nix or on https://nixos.org/nixos/options.html.
+  system.stateVersion = "22.11";
 }
