367ca1ced6987381726b50655aafcac678ec6017
geri1701 <geri@sdf.org>
2023-03-15 23:24:58 +0100

    remove boot from configuration.nix, add boot.nix

--- configuration.nix
+++ configuration.nix
@@ -34,23 +34,6 @@
       options = [ "defaults" "noatime" "nodiratime" ];
     };
   };
-  boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
-    initrd.kernelModules = [ "amdgpu" ];
-    kernelParams = [ "quiet" ];
-    cleanTmpDir = true;
-    plymouth.enable = true;
-    initrd.systemd.enable = true;
-    loader = {
-      efi.canTouchEfiVariables = true;
-      timeout = 1;
-      grub = {
-        enable = true;
-        device = "/dev/sda";
-        useOSProber = true;
-      };
-    };
-  };
   environment.sessionVariables.EDITOR = "hx";
   networking = {
     hostName = "zero";
