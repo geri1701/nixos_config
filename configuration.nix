cbb7f16600c01338ff64067d03aa78f097d8259e
geri1701 <geri@sdf.org>
2023-03-17 02:00:13 +0100

    Remove fonts from configuration.nix and add it to system-options.nix

--- configuration.nix
+++ configuration.nix
@@ -2,27 +2,8 @@
 { sops-nix, config, pkgs, ... }: {
   ### Enable touchpad support (enabled default in most desktopManager).
   ### services.xserver.libinput.enable = true;
-  fonts.fonts = with pkgs; [
-    dina-font
-    fantasque-sans-mono
-    fira-code
-    fira-code-symbols
-    liberation_ttf
-    meslo-lg
-    mplus-outline-fonts.githubRelease
-    nerdfonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    proggyfonts
-    tamsyn
-    tamzen
-    terminus_font
-  ];
   nixpkgs.config.allowUnfree = true;
   users.defaultUserShell = pkgs.zsh;
-  security.rtkit.enable = true;
-  security.polkit.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
