b163eef004635763978d8dafe41e5b58037d4683
geri1701 <geri@sdf.org>
2023-03-22 09:23:22 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url =
+      "github:hyprwm/Hyprland/37ced6aca4ad8f18f8e205e3cdb6de9c8964d4b6";
     sops-nix.url = "github:Mic92/sops-nix";
     stylix.url = "github:danth/stylix";
   };
