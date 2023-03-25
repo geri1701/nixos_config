4653e6b43b39a5599489d519f500e05571985bc9
geri1701 <geri@sdf.org>
2023-03-26 00:08:54 +0100

    Add waybar style

--- user-options.nix
+++ user-options.nix
@@ -293,6 +293,135 @@ in {
           };
         };
       };
+      style = ''
+                * {
+            border: none;
+            border-radius: 0;
+            font-family: Cartograph CF Nerd Font, monospace;
+            font-weight: bold;
+            font-size: 14px;
+            min-height: 0;
+        }
+
+        window#waybar {
+            background: rgba(21, 18, 27, 0);
+            color: #cdd6f4;
+        }
+
+        tooltip {
+            background: #002b36;
+            border-radius: 10px;
+            border-width: 2px;
+            border-style: solid;
+            border-color: #11111b;
+        }
+
+        #workspaces button {
+            padding: 5px;
+            color: #333333;
+            margin-right: 5px;
+            background: #002b36;    
+        }
+
+        #workspaces button.active {
+            color: #a6adc8;
+        }
+
+        #workspaces button.focused {
+            color: #a6adc8;
+            background: #002b36;
+            border-radius: 10px;
+        }
+
+        #workspaces button.urgent {
+            color: #11111b;
+            background: #002b36;
+            border-radius: 10px;
+        }
+
+        #workspaces button:hover {
+            background: #002b36; /**/
+            color: #002b36;
+            border-radius: 10px;
+        }
+
+        #custom-weather,
+        #window,
+        #clock,
+        #battery,
+        #pulseaudio,
+        #network,
+        #workspaces,
+        #tray {
+            background: #002b36;
+            padding: 0px 10px; 
+            margin-top: 3px;
+            border: 1px solid #181825;
+        }
+
+        #tray {
+            border-radius: 10px;
+            margin-right: 5px;
+        }
+
+        #workspaces {
+            background: #002b36;
+            border-radius: 10px;
+            margin-left: 10px;
+            margin-right: 5px;    
+            padding-right: 0px;
+            padding-left: 5px;
+        }
+
+        #custom-language {
+            color: #f38ba8;
+            border-left: 0px;
+            border-right: 0px;
+        }
+
+        #window {
+            border-radius: 10px;
+            margin-left: 60px;
+            margin-right: 60px;
+        }
+
+        #clock {
+            color: #fab387;
+            border-radius: 10px;
+            margin-left: 5px;
+            margin-right: 10px;    
+            border-right: 0px;
+        }
+
+        #network {
+            color: #f9e2af;
+            border-left: 0px;
+            border-right: 0px;
+            border-radius: 10px;    
+        }
+
+        #pulseaudio {
+            color: #89b4fa;
+            border-left: 0px;
+            border-right: 0px;
+            border-radius: 10px;
+            margin-right: 5px;        
+        }
+
+        #pulseaudio.microphone {
+            color: #cba6f7;
+            margin-right: 5px;
+            border-radius: 10px;
+        }
+
+        #custom-weather {
+            border-radius: 0px 10px 10px 0px;
+            border-right: 0px;
+            margin-left: 0px;
+            margin-right: 5px;    
+            border-radius: 10px;    
+        }
+      '';
     };
   };
   qt = {
