335f35e2804970a18b149d5a7cef86912178de48
geri1701 <geri@sdf.org>
2023-03-26 23:29:46 +0200

    Add waybar_exp to waybar.nix

--- config/waybar.nix
+++ config/waybar.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }:
+let
+  waybar_exp = (pkgs.waybar.overrideAttrs (oldAttrs: {
+    mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    postPatch = ''
+      substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
+    '';
+  }));
+in {
   programs.waybar = {
     enable = true;
     package = waybar_exp;
