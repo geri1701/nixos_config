12e54a6bfd19952d07dd6895b61afbef80e0bf40
geri1701 <geri@sdf.org>
2023-03-29 00:44:08 +0200

    Add solarized styleset to aerc.nix

--- config/aerc.nix
+++ config/aerc.nix
@@ -4,7 +4,7 @@
     extraConfig = {
       general = { unsafe-accounts-conf = true; };
       ui = {
-        styleset-name = "styleset";
+        styleset-name = "solarized";
         timestamp-format = "02 Jan 06 15:04:05";
       };
       viewer = { alternatives = "text/html,text/plain"; };
@@ -35,6 +35,58 @@
         from = "Gerhard <geri@sdf.org>";
         copy-to = "Sent";
       };
+    };
+    stylesets = {
+      solarized = ''
+        *.default=true
+        *error.bold=true
+        border.reverse=true
+        completion_pill.reverse=true
+        error.fg=#dc322f
+        header.bold=true
+        selector_chooser.bold=true
+        statusline*.default=true
+        success.fg=#859900
+        warning.fg=#b58900
+        *.selected.bg=#b58900
+        *.selected.fg=#3b4252
+        dirlist_recent.fg=#b58900
+        dirlist_unread.fg=#b58900
+        msglist_flagged.bold=true
+        msglist_flagged.fg=#a3be8c
+        msglist_marked.bg=#81a1c1
+        msglist_unread.fg=#b58900
+        msglist_unread.selected.bg=#b58900
+        msglist_unread.selected.fg =#002b36
+        statusline_*.bg=#859900
+        statusline_*.fg=#002b36
+        statusline_error.bg=#d33682
+        statusline_error.fg=#002b36
+        tab.bg=#eee8d5
+        tab.fg=#586e75
+        tab.selected.bg=#b58900
+        tab.selected.fg=#002b36
+        [viewer]
+        diff_add.fg=#859900
+        diff_chunk.dim=true
+        diff_chunk.fg=#2aa198
+        diff_del.fg=#dc322f
+        diff_meta.bold=true
+        diff_meta.fg=#839496
+        header.bold=true
+        header.fg=#d33682
+        quote_*.dim=true
+        quote_*.fg=#93a1a1
+        quote_1.dim=false
+        quote_1.fg=#268bd2
+        quote_2.fg=#cb4b16
+        quote_3.fg=#d33682
+        quote_4.fg=#6c71c4
+        signature.dim=true
+        signature.fg=#d33682
+        url.fg=#b58900
+        url.underline=true
+      '';
     };
   };
 }
