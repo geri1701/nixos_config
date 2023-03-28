588cc4de3f34a35cda622dfd1ffcc5ec8ca71a10
geri1701 <geri@sdf.org>
2023-03-28 23:13:54 +0200

    Fix typo in waybar.nix

--- config/waybar.nix
+++ config/waybar.nix
@@ -70,11 +70,10 @@ in {
           "states" = {
             "warning" = 30;
             "critical" = 15;
-            };
-            "format" = "{capacity}% {icon}";
-            "format-icons" = ["" "" "" "" ""];
-            "max-length" = 25;
           };
+          "format" = "{capacity}% {icon}";
+          "format-icons" = [ "" "" "" "" "" ];
+          "max-length" = 25;
         };
       };
     };
