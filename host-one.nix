eeed698b8ece7b5a0aa5bdd3cd0258c6427b93f0
geri1701 <geri@sdf.org>
2023-05-06 08:36:05 +0200

    Remove unused attributes and options

--- host-one.nix
+++ host-one.nix
@@ -1,12 +1,10 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{ config, lib, modulesPath, ... }:
 
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
   swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
