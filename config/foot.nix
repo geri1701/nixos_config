0da645d0c8db23fd82e1303c366e8390345eb6d0
geri1701 <geri@sdf.org>
2023-05-18 23:48:25 +0200

    Force alpha value in foot.nix

--- config/foot.nix
+++ config/foot.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: {
+{ lib, ... }: {
   programs.foot = {
     enable = true;
     settings = {
       main = { font = "Fantasque Sans Mono:size=10"; };
       mouse = { hide-when-typing = "yes"; };
-      colors = { alpha = 0.8; };
+      colors = { alpha = lib.mkForce 0.8; };
     };
   };
 }
