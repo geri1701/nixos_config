431927273585ed50bd46720d2a869fc2990050ee
geri1701 <geri@sdf.org>
2023-05-21 00:47:06 +0200

    Replace symbol

--- config/waybar.nix
+++ config/waybar.nix
@@ -28,9 +28,9 @@ in {
         };
         network = {
           format-wifi = "{essid} ({signalStrength}%) ";
-          format-ethernet = "{ipaddr}/{cidr} ";
-          tooltip-format = "{ifname} via {gwaddr} ";
-          format-linked = "{ifname} (No IP) ";
+          format-ethernet = "{ipaddr}/{cidr} 󱘖";
+          tooltip-format = "{ifname} via {gwaddr} 󱘖";
+          format-linked = "{ifname} (No IP) 󰌙";
           format-disconnected = "Disconnected ⚠";
           format-alt = "{ifname}: {ipaddr}/{cidr}";
         };
