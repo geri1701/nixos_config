9a37d6b64f9e5d2466a2ef8419a1e13797dd57d1
geri1701 <geri@sdf.org>
2024-01-09 21:12:54 +0100

    Change scale

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -3,7 +3,7 @@
     enable = true;
     xwayland.enable = true;
     extraConfig = ''
-      monitor=,highres,auto,1
+      monitor=,highres,auto,1.2
       exec-once=dunst
       exec-once=waybar 
       exec-once=swaybg -i /home/geri/nixos/wallpaper_logo.png
