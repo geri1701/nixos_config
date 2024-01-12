abf02c5349f97abad5159afe0c282c019762e4df
geri1701 <geri@sdf.org>
2024-01-12 17:49:14 +0100

    Change browser homepage

--- config/firefox.nix
+++ config/firefox.nix
@@ -7,7 +7,7 @@
       "browser.download.start_downloads_in_tmp_dir" = true;
       "layout.css.devPixelsPerPx" = "${config.firefoxPixelsPerPxOption}";
       "extensions.webextensions.tabhide.enabled" = true;
-      "browser.startup.homepage" = "localhost:9090/bookmarks";
+      "browser.startup.homepage" = "https://duckduckgo.com/";
       "browser.sessionstore.resume_from_crash" = false;
       "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
