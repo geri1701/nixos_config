92ff28b2f96efb28614f180061d546184a07b4ed
geri1701 <geri@sdf.org>
2024-03-16 01:33:45 +0100

    Change enableAutosuggestions to autosuggestions.enable

--- config/zsh.nix
+++ config/zsh.nix
@@ -4,7 +4,7 @@
     enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    enableAutosuggestions = true;
+    autosuggestions.enable = true;
     initExtraBeforeCompInit = ''
       fpath=($HOME/.zsh/ $fpath)
       # source $HOME/.zsh/_zellij
