38be578123732b94d833ccf076d49de2b4f5da52
geri1701 <geri@sdf.org>
2024-04-19 10:20:50 +0200

    Update flake.lock

--- one-boot.nix
+++ one-boot.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
     initrd = {
       systemd.enable = true;
       verbose = false;
