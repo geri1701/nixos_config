df4d99393d8c0116884ba5326d5715dedd0e3553
geri1701 <geri@sdf.org>
2024-07-07 18:55:46 +0200

    Remove joshuto

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
   inputs = {
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    joshuto.url = "github:kamiyaa/joshuto";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     stylix.url = "github:danth/stylix/2221c7d61b2e10b17df6c6795b4678fb59a0a92a";
